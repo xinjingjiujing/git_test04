d047ebee6a8c9017453100047491c47d88750b89
xinjingjiujing <138278674+xinjingjiujing@users.noreply.github.com>
2023-07-02 02:00:45 +0800

    Update Hello.java

--- src/main/java/git_test/Hello.java
+++ src/main/java/git_test/Hello.java
@@ -8,6 +8,8 @@ public class Hello{
         System.out.println("hello4");
         System.out.println("master test");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
+
 
     }
 }
