4822a71256f07bb424cd2e0239b0641bb436ae27
karl <2723905036@qq.com>
2023-07-02 01:05:18 +0800

    commit hot-fix02

--- src/main/java/git_test/Hello.java
+++ src/main/java/git_test/Hello.java
@@ -6,6 +6,7 @@ public class Hello{
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hello4");
+        System.out.println("hot-fix");
 
     }
 }
